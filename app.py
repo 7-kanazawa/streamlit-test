2c9fd2a77e01fd4291ff84fee2d2d47e3b24cdd7
7-kanazawa <kanazawa2024@gmail.com>
2024-08-25 15:18:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,7 @@ def get_each_score(row):
   list_lamp = ast.literal_eval(row['nearby_locat_街灯']) # 格納リストが文字列なのでリスト形式に変換
   len_hinanjo = len(row['nearby_locat_避難所'])
   list_noise = row['noise_levels'][0]
+  print("##########テスト###############")
   print(row['noise_levels'])
   
   if isinstance(list_noise, float) and np.isnan(list_noise):
