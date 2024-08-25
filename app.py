9fe45f4cead2b96747e80f8cb029916c177f0080
7-kanazawa <kanazawa2024@gmail.com>
2024-08-25 15:16:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ import ast
 def get_each_score(row):
   list_lamp = ast.literal_eval(row['nearby_locat_街灯']) # 格納リストが文字列なのでリスト形式に変換
   len_hinanjo = len(row['nearby_locat_避難所'])
-  list_noise = ast.literal_eval(row['noise_levels'])[0]
-
+  list_noise = row['noise_levels'][0]
+  print(row['noise_levels'])
   
   if isinstance(list_noise, float) and np.isnan(list_noise):
     avg_noise_level = np.nan
@@ -55,7 +55,6 @@ def get_each_score(row):
   return [len(list_lamp), avg_noise_level, len_hinanjo]
   # return [len(list_lamp), avg_noise_level]
 df_hanzai[['街灯の数', '騒音の平均値', '避難所の数']] = df_hanzai.apply(lambda x: pd.Series(get_each_score(x)), axis=1)
-
 
 
 ###1.住所から緯度経度情報を取得する
