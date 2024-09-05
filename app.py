8443ed192b95d7a2be07c460767e65a62b0f7d66
7-kanazawa <kanazawa2024@gmail.com>
2024-09-05 09:04:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -152,7 +152,11 @@ def get_coordinates(address):
 # GISデータから市区町村の境界データを取得
 import geopandas as gpd
 from shapely.geometry import Point
-gdf = gpd.read_file('./data/r2ka13.shp')
+
+try:
+    gdf = gpd.read_file('./data/r2ka13.shp')
+except Exception as e:
+    print(f"Error: {e}")
 
 ### 3. 住所が含まれる地域のポリゴンを抽出
 def get_polygon_for_address(address):
